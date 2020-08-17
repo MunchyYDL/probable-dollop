63102fcdc73cbdeed67298e0c5f522abc97203a8
Marcus Christensen <munchyydl@gmail.com>
2020-08-17 22:42:50 +0200

    Adds custom fonts

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,23 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-plugin-google-fonts-v2`,
+      options: {
+        fonts: [
+          {
+            family: 'Oswald',
+            variable: true,
+            weights: ['200..700']
+          },
+          {
+            family: 'Raleway',
+            variable: true,
+            weights: ['100..900', '100..900']
+          },
+        ]
+      }
+    },        
+  ],
 }
