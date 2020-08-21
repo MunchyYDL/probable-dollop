fed901093e4f4af2f05768583cc5bb1e6e399dc9
Marcus Christensen <munchyydl@gmail.com>
2020-08-21 12:34:33 +0200

    Mobile menu

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -6,16 +6,20 @@ import styles from "./navbar.module.css"
 export default function NavBar() {
 
     return (
-        // <footer className={`navbar ${styles.navbar}`}>
-            <nav className={styles.nav}>
-                <ul>
+        <nav className={styles.nav}>
+            <div className={styles.menuToggle}>
+                <input className={styles.input} type="checkbox"/>
+                <span className={styles.span}/>
+                <span className={styles.span}/>
+                <span className={styles.span}/>
+                <ul className={styles.menu}>
                     <li><Link className={styles.link} activeClassName={styles.activeLink} to="/">Hem</Link></li>
                     <li><Link className={styles.link} activeClassName={styles.activeLink} to="/what_we_do">Vad vi gör</Link></li>
                     <li><Link className={styles.link} activeClassName={styles.activeLink} to="/who_we_are">Vilka vi är</Link></li>
                     <li><Link className={styles.link} activeClassName={styles.activeLink} to="/where_we_are">Var vi finns</Link></li>
                     <li><Link className={styles.link} activeClassName={styles.activeLink} to="/contact">Kontakta oss!</Link></li>
                 </ul>
-            </nav>
-        // </footer>
+            </div>
+        </nav>
     )
 }
