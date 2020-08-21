de8dcec2aef0405ecd005eb011ef5be112298c02
Marcus Christensen <munchyydl@gmail.com>
2020-08-22 00:10:19 +0200

    Cleanup the contact.js file

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -24,7 +24,9 @@ export const query = graphql`
         }
       }`;
 
-const Test = ({data}) => (
+export default function Test({data}){
+    return (
+
         <Layout>
             <div className={`contact ${styles.contact}`}>
                 <h1>Kontakta oss!</h1>
@@ -55,42 +57,4 @@ const Test = ({data}) => (
             </div>
         </Layout>
     )
-
-export default Test
-
-// export default function Contact({data}) {
-
-//     return (
-//         <Layout>
-//             <div className={`contact ${styles.contact}`}>
-//                 <h1>Kontakta oss!</h1>
-//                 <p>Vi finns på plats på dessa orter, men tar även gärna uppdrag på distans.</p>
-//                 <div className={styles.addresses}>
-
-//                     <div>
-//                         <h2>Norrköping, Linköping, Stockholm</h2>
-//                         <img src={Erik} alt="Erik Fremred"/>
-//                         <Img fixed={data[0].childImageSharp.fixed} alt="Erik Fremred" />
-//                         <h3>Erik Fremred</h3>
-//                         <p>
-//                             +46 76 010 21 04<br/>
-//                             erik.fremred@devbrains.se
-//                         </p>
-//                     </div>
-
-//                     <div>
-//                         <h2>Göteborg</h2>
-//                         <img src={Sofie} alt="Sofie Askenbom"/>
-//                         <Img fixed={data.sofie.childImageSharp.fixed} alt="Sofie Askenbom" />
-//                         <h3>Sofie Askenbom</h3>
-//                         <p>
-//                             +46 70 874 20 71<br/>
-//                             sofie.askenbom@devbrains.se
-//                         </p>
-//                     </div>
-
-//                 </div>
-//             </div>
-//         </Layout>
-//     )
-// }
+}
