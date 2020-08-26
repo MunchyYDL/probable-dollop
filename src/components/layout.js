4829dc198a2b8284de83b50261d29a1348a8360b
Marcus Christensen <munchyydl@gmail.com>
2020-08-26 10:37:29 +0200

    Tweaked text-placement on the page And removed the background-image for now.

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,18 +1,33 @@
 import React from "react"
+// import { StaticQuery, graphql } from "gatsby"
+import BackgroundImage from "gatsby-background-image"
 
 import { NavBar } from "./"
 import styles from "./layout.module.css"
 
-export function Layout(props) {
+export function Layout( props ) {
+
+    const wrapper = props.background
+    ? (
+        <BackgroundImage Tag="div" className={styles.container} fluid={props.background}>
+            <NavBar />
+            <main>
+                {props.children}
+            </main>
+        </BackgroundImage>
+    )
+    : (
+        <div className={styles.container}>
+            <NavBar />
+            <main>
+                {props.children}
+            </main>
+        </div>
+    )
 
     return (
         <div className={`layout ${styles.layout}`}>
-            <div className={styles.container}>
-                <NavBar />
-                <main>
-                    {props.children}
-                </main>
-            </div>
+            {wrapper}
         </div>
     )
 }
