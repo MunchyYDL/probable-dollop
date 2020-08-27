9210631081ee68a91f36b8120afb34760d3b940d
Marcus Christensen <munchyydl@gmail.com>
2020-08-27 11:19:36 +0200

    Tweaks to colors and positioning

--- src/pages/who_we_are.js
+++ src/pages/who_we_are.js
@@ -31,17 +31,6 @@ export default function WhoWeAre({data}) {
                   <Person person={getPerson(people, "Andreas Ingman")}/>
                   <Person person={getPerson(people, "Johan Karlsteen")}/>
               </div>
-
-              {/* <p>
-                  Här har vi svartvita bilder på samtliga utvecklare + projektledare grupperat efter ort.
-                  Det ska finnas en personlig text och tekniska kunskaper under varje bild.
-                  Och under mig och Erik ska det finnas kontaktinformation, mail och telefon.
-              </p> */}
-              {/* <p>
-                  Längst ned i detta avsnitt ska det finnas en rubrik som länkar till ett enkelt formulär
-                  där man skriver sina kontaktuppgifter, laddar upp sitt cv (pdf, word) samt en ruta for meddelanden.
-                  Formuläret får gärna skickas till både min och Eriks mail.
-              </p> */}
             </div>
         </Layout>
     )
