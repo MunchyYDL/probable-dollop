a7f1eff067228549d12ec16502eecf04777c3e75
Marcus Christensen <munchyydl@gmail.com>
2020-08-27 13:48:37 +0200

    Add map and tweak some settings

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -5,21 +5,21 @@ import styles from "./navbar.module.css"
 
 export function NavBar() {
 
-    return (
-        <nav className={styles.nav}>
-            <div className={styles.menuToggle}>
-                <input className={styles.input} type="checkbox"/>
-                <span className={styles.span}/>
-                <span className={styles.span}/>
-                <span className={styles.span}/>
-                <ul className={styles.menu}>
-                    <li><Link className={styles.link} activeClassName={styles.activeLink} to="/">Hem</Link></li>
-                    <li><Link className={styles.link} activeClassName={styles.activeLink} to="/what_we_do">Vad vi gör</Link></li>
-                    <li><Link className={styles.link} activeClassName={styles.activeLink} to="/who_we_are">Vilka vi är</Link></li>
-                    <li><Link className={styles.link} activeClassName={styles.activeLink} to="/where_we_are">Var vi finns</Link></li>
-                    <li><Link className={styles.link} activeClassName={styles.activeLink} to="/contact">Kontakta oss!</Link></li>
-                </ul>
-            </div>
-        </nav>
-    )
+  return (
+    <nav className={styles.nav}>
+      <div className={styles.menuToggle}>
+        <input className={styles.input} type="checkbox" />
+        <span className={styles.span} />
+        <span className={styles.span} />
+        <span className={styles.span} />
+          <ul className={styles.menu}>
+            <li><Link className={styles.link} activeClassName={styles.activeLink} to="/">Hem</Link></li>
+            <li><Link className={styles.link} activeClassName={styles.activeLink} to="/what_we_do">Vad vi gör</Link></li>
+            <li><Link className={styles.link} activeClassName={styles.activeLink} to="/who_we_are">Vilka vi är</Link></li>
+            <li><Link className={styles.link} activeClassName={styles.activeLink} to="/where_we_are">Var vi finns</Link></li>
+            <li><Link className={styles.link} activeClassName={styles.activeLink} to="/contact">Kontakta oss!</Link></li>
+          </ul>
+      </div>
+    </nav>
+  )
 }
