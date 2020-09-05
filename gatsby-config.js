f90127c8dc34a83d7fc66663b55a311e2b42c11e
Marcus Christensen <munchyydl@gmail.com>
2020-09-05 12:24:45 +0200

    Add simple page transitions

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,7 @@
 module.exports = {
   /* Your site config here */
   plugins: [
+    `gatsby-plugin-transition-link`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Link } from "gatsby"
+import AniLink from "gatsby-plugin-transition-link/AniLink";
 
 import styles from "./navbar.module.css"
 
@@ -13,11 +13,11 @@ export function NavBar() {
         <span className={styles.span} />
         <span className={styles.span} />
           <ul className={styles.menu}>
-            <li><Link className={styles.link} activeClassName={styles.activeLink} to="/">Hem</Link></li>
-            <li><Link className={styles.link} activeClassName={styles.activeLink} to="/what_we_do">Vad vi gör</Link></li>
-            <li><Link className={styles.link} activeClassName={styles.activeLink} to="/who_we_are">Vilka vi är</Link></li>
-            <li><Link className={styles.link} activeClassName={styles.activeLink} to="/where_we_are">Var vi finns</Link></li>
-            <li><Link className={styles.link} activeClassName={styles.activeLink} to="/contact">Kontakta oss!</Link></li>
+            <li><AniLink fade className={styles.link} activeClassName={styles.activeLink} to="/">Hem</AniLink></li>
+            <li><AniLink fade className={styles.link} activeClassName={styles.activeLink} to="/what_we_do">Vad vi gör</AniLink></li>
+            <li><AniLink fade className={styles.link} activeClassName={styles.activeLink} to="/who_we_are">Vilka vi är</AniLink></li>
+            <li><AniLink fade className={styles.link} activeClassName={styles.activeLink} to="/where_we_are">Var vi finns</AniLink></li>
+            <li><AniLink fade className={styles.link} activeClassName={styles.activeLink} to="/contact">Kontakta oss!</AniLink></li>
           </ul>
       </div>
     </nav>
